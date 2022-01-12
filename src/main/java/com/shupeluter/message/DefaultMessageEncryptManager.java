8d0baa40964b6575ec8c3d004054495732ec9e59
Yosihiro Saitoh <shupeluter@hotmail.com>
2022-01-13 08:16:48 +0900

    secret鍵の処理を開始

--- src/main/java/com/shupeluter/message/DefaultMessageEncryptManager.java
+++ src/main/java/com/shupeluter/message/DefaultMessageEncryptManager.java
@@ -6,13 +6,16 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 import java.security.spec.X509EncodedKeySpec;
 
 public class DefaultMessageEncryptManager extends AbsMessageEncryptManager {
-    String KEY_PUBLIC="src/test/resources/keys/public.der";
+    String KEY_PUBLIC = "src/test/resources/keys/public.der";
+    String KEY_PRIVATE = "src/test/resources/keys/private.pk8";
+    String SECRET_KEY_ID = "secret";
 
     @Override
     /**
@@ -22,25 +25,37 @@ public class DefaultMessageEncryptManager extends AbsMessageEncryptManager {
         Path keyFile = this.getFile(keyId);
         KeySpec keySpec = null;
         KeyFactory keyFactory;
-        PublicKey publicKey=null;
+        PublicKey publicKey = null;
 
         try {
             keySpec = new X509EncodedKeySpec(Files.readAllBytes(keyFile));
             keyFactory = KeyFactory.getInstance("RSA");
-            publicKey =  keyFactory.generatePublic(keySpec);
-            
+            publicKey = keyFactory.generatePublic(keySpec);
+
         } catch (NoSuchAlgorithmException | InvalidKeySpecException | IOException e) {
-             e.printStackTrace();
+            e.printStackTrace();
         }
 
         return publicKey;
 
     }
 
+    @Override
+    public PrivateKey getPrivateKey() {
+        Path keyFile = this.getFile(SECRET_KEY_ID);
+        KeySpec keySpec = null;
+        KeyFactory keyFactory;
+        PrivateKey privateKey=null;
+        
+        return privateKey;
+    }
 
-
-   protected Path getFile(String keyId){
-       //FIXME 処理は後から追加する
-       return Paths.get(KEY_PUBLIC);
-   } 
+    protected Path getFile(String keyId) {
+        // FIXME 処理は後から追加する
+        if (SECRET_KEY_ID.equals(keyId)) {
+            return Paths.get(Key)
+        } else {
+            return Paths.get(KEY_PUBLIC);
+        }
+    }
 }
--- src/main/java/com/shupeluter/message/MessageEncyptManager.java
+++ src/main/java/com/shupeluter/message/MessageEncyptManager.java
@@ -1,5 +1,6 @@
 package com.shupeluter.message;
 
+import java.security.PrivateKey;
 import java.security.PublicKey;
 
 public interface MessageEncyptManager {
@@ -9,5 +10,10 @@ public interface MessageEncyptManager {
      * keyIdで一意に決定される共通鍵を返却する。
      */
     PublicKey getPublickey(String keyId);
+
+    /***
+     * 自分の秘密鍵を返却する
+     */
+    PrivateKey getPrivateKey();
 
 }
