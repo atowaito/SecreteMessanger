2df0164dd57b7656ced832a802ac676a7f99a37d
Yosihiro Saitoh <shupeluter@hotmail.com>
2022-01-16 19:27:57 +0900

    最新化

---
+++ src/main/java/com/shupeluter/message/DefaultMessageEncrpytManager.java
@@ -0,0 +1,20 @@
+package com.shupeluter.message;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public class DefaultMessageEncrpytManager extends AbsMessageEncryptManager{
+
+    @Override
+    public PublicKey getPublickey(String keyId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public PrivateKey getPrivateKey(String keyId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
